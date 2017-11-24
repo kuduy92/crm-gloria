496c18ca601c67ba1ae2659ca8571d24f9566517
Prasetyo Wildan Aulia <dudepwa@gmail.com>
2017-11-24 15:21:45 +0700

    demo commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel -- Contributor: Prasetyo Wildan AUlia
                 </div>
 
                 <div class="links">
